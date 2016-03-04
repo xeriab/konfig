a7a1ccd3b24f77c3bd85cfc36641dc3ed107b8d7
Xeriab Nabil <kodeburner@gmail.com>
2016-03-04 03:11:21 +0300

    Adding Tests

---
+++ tests/mocks/pass/config-exec.php
@@ -0,0 +1,16 @@
+<?php
+return function () {
+    return array(
+        'host' => 'localhost',
+        'port' => 80,
+        'servers' => array(
+            'host1',
+            'host2',
+            'host3',
+        ),
+        'app' => array(
+            'name' => 'konfig',
+            'secret' => 'secret',
+        ),
+    );
+};
