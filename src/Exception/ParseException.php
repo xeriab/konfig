709c8e9cb9d1134ac0865622f944e73ae739cdd6
Xeriab Nabil <kodeburner@gmail.com>
2016-03-08 00:15:46 +0300

    Fixing code style

--- src/Exception/ParseException.php
+++ src/Exception/ParseException.php
@@ -17,11 +17,11 @@ class ParseException extends ErrorException
 {
     public function __construct(array $error)
     {
-        $message = $error['message'] ?: 'There was an error parsing the file';
-        $code = isset($error['code']) ? $error['code'] : 0;
-        $severity = isset($error['type']) ? $error['type'] : 1;
-        $fileName = isset($error['file']) ? $error['file'] : __FILE__;
-        $lineNo = isset($error['line']) ? $error['line'] : __LINE__;
+        $message   = $error['message'] ?: 'There was an error parsing the file';
+        $code      = isset($error['code']) ? $error['code'] : 0;
+        $severity  = isset($error['type']) ? $error['type'] : 1;
+        $fileName  = isset($error['file']) ? $error['file'] : __FILE__;
+        $lineNo    = isset($error['line']) ? $error['line'] : __LINE__;
         $exception = isset($error['exception']) ? $error['exception'] : null;
 
         parent::__construct($message, $code, $severity, $fileName, $lineNo, $exception);
