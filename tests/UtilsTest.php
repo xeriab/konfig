d64795a7498c75aca5d1bf7c1f0cc73c102f9e32
Xeriab Nabil <kodeburner@gmail.com>
2016-03-14 05:25:58 +0300

    Adding Utils test

---
+++ tests/UtilsTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Konfig
+ *
+ * Yet another simple configuration loader library.
+ *
+ * @author  Xeriab Nabil (aka KodeBurner) <kodeburner@gmail.com>
+ * @license https://raw.github.com/xeriab/konfig/master/LICENSE MIT
+ * @link    https://xeriab.github.io/projects/konfig
+ */
+
+namespace Exen\Konfig\Test;
+
+use Exen\Konfig\Utils;
+
+/**
+ * Tests for Exen\Konfig\Utils.
+ *
+ * @package Test
+ * @subpackage Utils
+ */
+class UtilsTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        // Nothing to put here
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+        // Nothing to put here
+    }
+}
+
+#: END OF ./tests/UtilsTest.php FILE
