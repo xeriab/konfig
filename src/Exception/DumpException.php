83bf2163fc29b61a14003ec181dd0a717ef6d829
Xeriab Nabil <kodeburner@gmail.com>
2016-10-12 00:34:13 +0300

    Added more exceptions

---
+++ src/Exception/DumpException.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Konfig
+ *
+ * Yet another simple configuration loader library.
+ *
+ * @author  Xeriab Nabil (aka KodeBurner) <kodeburner@gmail.com>
+ * @license https://raw.github.com/xeriab/konfig/master/LICENSE MIT
+ * @link    https://xeriab.github.io/projects/konfig
+ */
+
+namespace Exen\Konfig\Exception;
+
+class DumpException extends RuntimeException
+{
+    // Nothing to put here!
+}
+
+// END OF ./src/Exception/DumpException.php FILE
---
+++ src/Exception/RuntimeException.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Konfig
+ *
+ * Yet another simple configuration loader library.
+ *
+ * @author  Xeriab Nabil (aka KodeBurner) <kodeburner@gmail.com>
+ * @license https://raw.github.com/xeriab/konfig/master/LICENSE MIT
+ * @link    https://xeriab.github.io/projects/konfig
+ */
+
+namespace Exen\Konfig\Exception;
+
+class RuntimeException extends \RuntimeException implements ExceptionInterface
+{
+    // Nothing to put here
+}
+
+// END OF ./src/Exception/RuntimeException.php FILE
