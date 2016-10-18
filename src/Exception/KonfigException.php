0c1400570e3ff8e541b836465336bf99ba76854b
Xeriab Nabil <kodeburner@gmail.com>
2016-10-18 11:38:50 +0300

    Renamed it to clarify the code a little bit

---
+++ src/Exception/KonfigException.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Konfig.
+ *
+ * Yet another simple configuration loader library.
+ *
+ * PHP version 5
+ *
+ * @category Library
+ * @package  Konfig
+ * @author   Xeriab Nabil (aka KodeBurner) <kodeburner@gmail.com>
+ * @license  https://raw.github.com/xeriab/konfig/master/LICENSE MIT
+ * @link     https://xeriab.github.io/projects/konfig
+ */
+
+namespace Exen\Konfig\Exception;
+
+/**
+ * KonfigException.
+ *
+ * Konfig exception class
+ *
+ * @category Exception.
+ * @package  Konfig
+ * @author   Xeriab Nabil (aka KodeBurner) <kodeburner@gmail.com>
+ * @license  https://raw.github.com/xeriab/konfig/master/LICENSE MIT
+ * @link     https://xeriab.github.io/projects/konfig
+ */
+class KonfigException extends \Exception
+{
+    // Nothing to put here!
+}
+
+// END OF ./src/Exception/KonfigException.php FILE
